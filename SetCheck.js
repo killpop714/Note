b00e57407f62c7029d0cd35bc23dc77ca65c2c30
killpop714 <112853297+killpop714@users.noreply.github.com>
2024-11-23 11:41:38 +0900

    기본 파일

---
+++ SetCheck.js
@@ -0,0 +1,14 @@
+
+
+function SetCheck(){
+    var start = document.querySelector("label");
+    var end = document.querySelector("#circle")
+    if(start.addEventListener("animationstart",listener,false))
+    {
+        {
+    }
+    
+}
+
+
+
